b741737cceed3525889ce8c373105cb5bcce27de
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-11-24 19:45:09 -0600

    pseudocode for setting initial state in reducer

--- actions/index.js
+++ actions/index.js
@@ -15,3 +15,16 @@ export function addDeck (deck) {
         deck
     }
 }
+
+ /* 
+
+ PSEUDOCODE 
+ how to solve this - we are going to set the initial state in the reducer. 
+ TO DO THIS - create store must have a second argument, else it is undefined 
+ steps: 
+ 1) make a root reducer 
+ 2) all other reducers should be in their separate files and imported into the root reducer
+ 3) where you set up the store, import root reducer -instead of reducer as arg, use root reducer
+
+ 
+ */ 
